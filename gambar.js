c2aeb4a3d692c84bcca0d223826f2ff4247e587c
Giraken <89152100+Giraken@users.noreply.github.com>
2023-07-18 11:05:22 +0700

    Finished

--- gambar.js
+++ gambar.js
@@ -30,24 +30,24 @@ window.onload = ()=>{ //after window loaded
 
 //fullscreen image preview function
 //selecting all required elements
-const previewBox = document.querySelector(".preview-box"),
-categoryName = previewBox.querySelector(".title p"),
-previewImg = previewBox.querySelector("img"),
-closeIcon = previewBox.querySelector(".icon"),
-shadow = document.querySelector(".shadow");
+// const previewBox = document.querySelector(".preview-box"),
+// categoryName = previewBox.querySelector(".title p"),
+// previewImg = previewBox.querySelector("img"),
+// closeIcon = previewBox.querySelector(".icon"),
+// shadow = document.querySelector(".shadow");
 
-function preview(element){
-    //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
-    document.querySelector("body").style.overflow = "hidden";
-    let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
-    let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
-    previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
-    categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
-    previewBox.classList.add("show"); //show the preview image box
-    shadow.classList.add("show"); //show the light grey background
-    closeIcon.onclick = ()=>{ //if user click on close icon of preview box
-        previewBox.classList.remove("show"); //hide the preview box
-        shadow.classList.remove("show"); //hide the light grey background
-        document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
-    }
-}
+// function preview(element){
+//     //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+//     document.querySelector("body").style.overflow = "hidden";
+//     let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+//     let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+//     previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+//     categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+//     previewBox.classList.add("show"); //show the preview image box
+//     shadow.classList.add("show"); //show the light grey background
+//     closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+//         previewBox.classList.remove("show"); //hide the preview box
+//         shadow.classList.remove("show"); //hide the light grey background
+//         document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+//     }
+// }
